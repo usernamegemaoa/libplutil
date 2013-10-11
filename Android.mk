1e20a3237ae1d71504c0a25549e0789326b0a240
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2013-10-11 18:11:03 +0100

    Android.mk: fix path to eglib

--- Android.mk
+++ Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libplutil
 LOCAL_SRC_FILES := plconfig.c
 LOCAL_CFLAGS += -Wall -O2
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../external/eglib
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../external/eglib
 include $(BUILD_STATIC_LIBRARY)
